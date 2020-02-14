641de318543f4579c17e9b63c9ec573dcd9e44bb
Summer <sgbanister@gmail.com>
2020-02-13 20:07:59 -0800

    Edited Blackjack

---
+++ Blackjack/Assets/Scripts/GameAI.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAI : MonoBehaviour
+{
+    public static string[] suits = new string[] { "C", "H", "D", "S" };
+}
