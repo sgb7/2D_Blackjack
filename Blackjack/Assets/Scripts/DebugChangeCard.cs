9ffbd719f5dded4e056fbbdc234f2436730cb97f
Summer <sgbanister@gmail.com>
2020-02-17 12:15:45 -0800

    Edited 2D_Blackjack

---
+++ Blackjack/Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    CardModel cardModel;
+    int cardIndex = 0;
+
+    public GameObject card;
+
+    void Awake()
+    {
+        cardModel = card.GetComponent<CardModel>();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 100, 28), "Hit me!"))
+        {
+            cardModel.cardIndex = cardIndex;
+            cardModel.ToggleFace(true);
+
+            if(cardIndex == 52)
+            {
+                cardIndex = 0;
+                cardModel.ToggleFace(false);
+            }
+            else
+            {
+                cardIndex++;
+            }
+        }
+    }
+}
+
+//Debug code needs debugging
