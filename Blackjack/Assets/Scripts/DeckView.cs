db7ac9ae3568366f722d6decfe2d00b4205afb5a
Summer <sgbanister@gmail.com>
2020-02-17 19:27:48 -0800

    Edited 2D_Blackjack

---
+++ Blackjack/Assets/Scripts/DeckView.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DeckModel))]
+public class DeckView : MonoBehaviour
+{
+    DeckModel deck;
+
+    public Vector3 start;
+    public float cardOffset;
+    public GameObject cardPrefab;
+
+    void Start()
+    {
+        deck = GetComponent<DeckModel>();
+        ShowCards();
+    }
+
+    void ShowCards()
+    {
+        int cardCount = 0;
+
+        foreach(int i in deck.GetCards())
+        {
+            float co = cardOffset * cardCount;
+
+            GameObject cardCopy = (GameObject)Instantiate(cardPrefab);
+            Vector3 temp = start + new Vector3(co, 0f);
+            cardCopy.transform.position = temp;
+
+            CardModel cardModel = cardCopy.GetComponent<CardModel>();
+            cardModel.cardIndex = i;
+            cardModel.ToggleFace(true);
+
+            cardCount++;
+        }
+    }
+}
